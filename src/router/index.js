27674a0abf3ff705319d4002fa5d53d9c84e730d
Alisha Kodibagkar <46933681+alishakodibagkar@users.noreply.github.com>
2023-03-12 16:37:21 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -33,28 +33,43 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
 
   var query_dict = {};
-  console.log(query_dict);
 
   if (from.query.auth_token && !to.query.auth_token) {
     query_dict["auth_token"] = from.query.auth_token;
-    console.log(query_dict);
-
   }
 
   if (from.query.expiry_time && !to.query.expiry_time) {
     query_dict["expiry_time"] = from.query.expiry_time;
-    console.log(query_dict);
   }
   
   if (from.query.uid && !to.query.uid) {
     query_dict["uid"] = from.query.uid;
-    console.log(query_dict);
+  }
+  
+  if (from.query.lang && !to.query.lang) {
+    query_dict["lang"] = from.query.lang;
+  }
+  
+  if (from.query.questions && !to.query.questions) {
+    query_dict["questions"] = from.query.questions;
+  }
+  
+  if (from.query.branch && !to.query.branch) {
+    query_dict["branch"] = from.query.branch;
+  }
+  
+  if (from.query.day && !to.query.day) {
+    query_dict["day"] = from.query.day;
   }
   
 
   if ((from.query.auth_token && !to.query.auth_token) 
       || (from.query.expiry_time && !to.query.expiry_time) 
-      || (from.query.uid && !to.query.uid)) {
+      || (from.query.uid && !to.query.uid)
+      || (from.query.lang && !to.query.lang)
+      || (from.query.questions && !to.query.questions)
+      || (from.query.branch && !to.query.branch)
+      || (from.query.day && !to.query.day)){
     if (from.path === to.path) {
       next(false);
     } else {
@@ -66,54 +81,6 @@ router.beforeEach((to, from, next) => {
   } else {
     next();
   }
-
-  if (from.query.expiry_time && !to.query.expiry_time ) {
-    if (from.path === to.path) {
-      next(false);
-    } else {
-      next({
-        path: to.path,
-        query: query_dict
-      });
-    }
-  } else {
-    next();
-  }
-  
-// router.beforeEach((to, from, next) => {
-  
-//   var query_dict = {};
-//   console.log(query_dict);
-  
-//   if (from.query.auth_token && !to.query.auth_token) {
-//     if (from.path === to.path) {
-//       next(false);
-//     } else {
-//       query_dict["auth_token"] = from.query.auth_token;
-//       console.log(query_dict);
-//       next({
-//         path: to.path,
-//         query: {...to.query, auth_token: from.query.auth_token},
-//       });
-//     }
-//   } else {
-//     next();
-//   }
-  
-//   if (from.query.expiry_time && !to.query.expiry_time) {
-//     if (from.path === to.path) {
-//       next(false);
-//     } else {
-//       query_dict["expiry_time"] = from.query.expiry_time;
-//       console.log(query_dict);
-//       next({
-//         path: to.path,
-//         query: {...to.query, expiry_time: from.query.expiry_time, auth_token: from.query.auth_token}
-//       });
-//     }
-//   } else {
-//     next();
-//   }
 
 
 })
